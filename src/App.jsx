8ac11c84338bfd03f660ddf9638cba8dea978ad5
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-06 09:30:55 +0900

    Navbar및 라우트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  return <div className="App">앱입니다</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
