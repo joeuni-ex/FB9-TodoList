55e327907492e0932a78b378ed23ce61922b98f6
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-06 10:27:29 +0900

    getDocs-> snapshot( 실시간 반영)으로 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import TodoList from "../components/TodoList.jsx";
 import TodoForm from "../components/TodoForm.jsx";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase/config.js";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
     const ref = collection(db, "todos"); //firebase config의 db임, todos가져오기
 
     //todos컬렉션에 모든 문서들을 가져오기
-    getDocs(ref).then((snapshot) => {
+    const unsub = onSnapshot(ref, (snapshot) => {
       let results = [];
       //성공 시 .docs의 모든 문서들이 들어있음
       snapshot.docs.forEach((doc) => {
@@ -20,6 +20,7 @@ export default function Home() {
       });
       setTodos(results);
     });
+    return () => unsub();
   }, []); //한 번만 실행
 
   return (
