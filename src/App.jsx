14c0d6401dfc4e6806f92b2222b3e3a9e0e20814
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-06 09:21:06 +0900

    파일 정리 , index.css교체

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>앱입니다</>;
+  return <div className="App">앱입니다</div>;
 }
 
 export default App;
