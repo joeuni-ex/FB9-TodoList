197ba254445144452152313b8f2024c25c4eb2a8
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-06 10:41:21 +0900

    삭제하기

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,9 @@
-import { deleteDoc } from "firebase/firestore";
+import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 export default function TodoList({ todos }) {
   const handleClick = async (id) => {
-    const ref = collection(db, "todos"); //db config 추가하기
+    const ref = doc(db, "todos", id); //db config 추가하기
     await deleteDoc(ref);
   };
 
